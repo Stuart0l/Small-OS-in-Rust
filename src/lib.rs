cf1a50f41f648dc8ec370017704bebbee235939e
Xuhao Luo <rossihox@gmail.com>
2019-12-30 21:43:57 -0800

    add a implementation for println

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,16 @@
 #![no_std]
 
+extern crate spin;
+extern crate lazy_static;
+
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start()
 {
-	let hello = b"hello world!";
-	let color_byte: u8 = 0x1f;
-	let buffer_ptr = (0xb8000) as *mut u8;
-
-	for (i, &char_byte) in hello.iter().enumerate() {
-		unsafe {
-			*buffer_ptr.offset(i as isize * 2) = char_byte;
-			*buffer_ptr.offset(i as isize * 2 + 1) = color_byte;
-		}
-	}
+	println!("hello world{}", '!');
 
 	loop {}
 }
